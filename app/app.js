eabb4924112bf5407ac770d8dc85c72a72352b06
Kevin Boucher <kevin@kevinboucher.com>
2018-10-11 10:51:54 -0500

    V1 updates (new resume PDF, removes broken animation)

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import loadInitializers from 'ember-load-initializers';
 import Resolver from './resolver';
 import config from './config/environment';
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
+// Ember.MODEL_FACTORY_INJECTIONS = true;
 
 const App = Ember.Application.extend({
     modulePrefix: config.modulePrefix,
